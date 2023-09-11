2821fa15342c6a28b150c200c447137cfc835bb7
koudstaal <skingabrian@gmail.com>
2023-09-11 02:07:23 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: Prints a specific text with proper grammar.
+ *     * The outcome is a piece of art.
+ *      *
+ *       * Return: Always 0 (Success)
+ *        */
 int main(void)
 {
-	    printf("with proper grammar, but the outcome is a piece of art,\n");
-	        return (0);
+		/* Using printf to display the desired text */
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
 }
 
